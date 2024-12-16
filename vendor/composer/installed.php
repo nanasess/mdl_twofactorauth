70dc8f61e752e3dbe283fb8d024be8c6d978ba3f
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-12-16 11:59:27 +0900

    Add TwoFactorAuth18

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f8a788070cf3ec4cee2fea4051a9b7ca6be48370',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f8a788070cf3ec4cee2fea4051a9b7ca6be48370',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nanasess/twofactorauth18' => array(
+            'pretty_version' => '1.8.2',
+            'version' => '1.8.2.0',
+            'reference' => '65681de5a324eae05140ac58b08648a60212afc0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nanasess/twofactorauth18',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
